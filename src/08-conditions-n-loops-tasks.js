8f8e04a7a3ec1c9d177535006754eb81f6b848b8
OKuts <kuts.ol.vas@gmail.com>
2020-12-10 17:36:33 +0200

    feat: 50% loops

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -27,8 +27,11 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (!(num % 5) || !(num % 3)) {
+    return (!(num % 3) ? 'Fizz' : '') + (!(num % 5) ? 'Buzz' : '');
+  }
+  return num;
 }
 
 
@@ -43,8 +46,8 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  return (n !== 1) ? n * getFactorial(n - 1) : 1;
 }
 
 
@@ -60,8 +63,8 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  return new Array(n2 - n1 + 1).fill(0).reduce((acc, el, i) => acc + n1 + i, 0);
 }
 
 
@@ -117,8 +120,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const xLeft = Math.max(rect1.left, rect2.left);
+  const yTop = Math.max(rect1.top, rect2.top);
+  const xRight = Math.min(rect1.left + rect1.width, rect2.left + rect2.width);
+  const yBottom = Math.min(rect1.top + rect1.height, rect2.top + rect2.height);
+  return xLeft <= xRight && yTop <= yBottom;
 }
 
 
@@ -148,8 +155,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const c = ((circle.center.x - point.x) ** 2 + (circle.center.y - point.y) ** 2) ** (1 / 2);
+  return c < circle.radius;
 }
 
 
@@ -164,8 +172,8 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  return str.split('').filter((el, i, arr) => arr.indexOf(el) === arr.lastIndexOf(el))[0] || null;
 }
 
 
@@ -191,8 +199,8 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  return (isStartIncluded ? '[' : '(') + ([a, b].sort((i, j) => i - j)).join(', ') + (isEndIncluded ? ']' : ')');
 }
 
 
@@ -208,8 +216,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -225,8 +233,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return Number(String(num).split('').reverse().join(''));
 }
 
 
@@ -250,8 +258,16 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const number = String(ccn);
+  const odd = number.length % 2;
+  let sumControl = Number(number[number.length - 1]);
+  number.split('').slice(0, -1).forEach((el, i) => {
+    if (i % 2 === odd) {
+      sumControl += el * 2 > 9 ? el * 2 - 9 : el * 2;
+    } else sumControl += Number(el);
+  });
+  return !(sumControl % 10);
 }
 
 /**
@@ -268,8 +284,12 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let result = num;
+  while (result > 9) {
+    result = String(result).split('').reduce((acc, el) => acc + Number(el), 0);
+  }
+  return result;
 }
 
 
